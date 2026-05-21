b7e972b24df93d160cdee912ab755d309e275a73
Sree <raksharaoivn2000@gmail.com>
2026-05-21 22:35:27 +0530

    New account creation and passbook reader

---
+++ Passbook.py
@@ -0,0 +1,19 @@
+# All the bank calculations will from now on be done here as the main program. 
+def filewriter(Username,Age) :
+    with open("Account.txt", "a") as f:
+        f.write(f"{Username}, {Age} \n")
+    
+def AccountCreation():
+    Username = input("Enter your User Name: ")
+    Age = int(input("Enter your Age: "))
+    if Age <= 16 :
+        print("Not eligible : You should be older than 16 years to create account ")
+    elif Age > 16 :
+        print("Congratulations you are eligible to create account")
+        NewAccount = input(" Would you like to create account(Yes/No): ")
+        if NewAccount.lower() == "yes":
+            filewriter (Username, Age)
+AccountCreation()
+
+
+
