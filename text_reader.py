30f7762265b3e3155ed5ccf0d9597d0c5b686331
Abhi <abhi2632001@gmail.com>
2026-05-21 22:06:33 +0530

    Enhanced text_reader.py

--- text_reader.py
+++ text_reader.py
@@ -1,4 +1,22 @@
+# necessary imports
+import re
+
+# input for dynamic path
 path = input("Enter path of the text or log file : ").strip("'\"")
 
-with open(path) as f:
-    print(f.read())
+# validation function to check if the give link is of .txt or .log
+if re.findall(".log$|.txt$", path):
+    with open(path) as f:
+        data = f.read().lower()
+        start_text = input("Enter start text : ").lower()
+        End_text = input("Enter end text : ").lower()
+        pattern = fr"{re.escape(start_text)}(.*?)(?={re.escape(End_text)})"
+        match = re.search(pattern, data,re.DOTALL) 
+        if match:
+            # .strip() removes any leading or trailing whitespace/newlines
+            extracted_data = match.group(1).strip()
+            print(extracted_data)
+        else:
+            print("Did not find specified pattern in the given context.")
+else:
+    print("Not a .txt or .log file")
